f239d3e1f2910d34a5079549bb9e482005986031
jonesdeini <jonesdeini@gmail.com>
2014-04-23 02:03:34 -0400

    Add ROM and setup the database

--- Gemfile
+++ Gemfile
@@ -4,6 +4,12 @@ gem "haml", "~> 4.0.5"
 gem 'sinatra', '~> 1.4.4'
 gem 'unicorn', '~> 4.8.1'
 
+#ROM stuff
+gem 'axiom', '~> 0.2'
+gem 'axiom-do-adapter', '~> 0.2'
+gem 'do_postgres'
+gem 'rom', '~> 0.2', git: 'git@github.com:rom-rb/rom.git', branch: 'master'
+
 group :development do
   gem 'capistrano'
   gem 'rvm-capistrano'
---
+++ db/setup.rb
@@ -0,0 +1,32 @@
+require 'do_postgres'
+require_relative 'seakrets'
+
+DataObjects.logger.set_log('log/do.log', :debug)
+
+def setup_db
+  connection = DataObjects::Connection.new("postgres://#{PG_USER}:#{PG_PASS}@localhost/#{PG_DATABASE}")
+
+  connection.create_command('DROP TABLE IF EXISTS "posts"').execute_non_query
+  connection.create_command('DROP TABLE IF EXISTS "comments"').execute_non_query
+
+  connection.create_command(<<-SQL.gsub(/\s+/, ' ').strip).execute_non_query
+    CREATE TABLE comments (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      post_id INTEGER NOT NULL,
+      email VARCHAR(255),
+      message TEXT
+    )
+  SQL
+
+  connection.create_command(<<-SQL.gsub(/\s+/, ' ').strip).execute_non_query
+    CREATE TABLE posts (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title VARCHAR(255),
+      body TEXT
+    )
+  SQL
+
+  connection.close
+end
+
+setup_db
