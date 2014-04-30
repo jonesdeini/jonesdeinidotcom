43e1b5ebad5ba6c3e0a1c7ab3314bd5a06eaa8d5
jonesdeini <jonesdeini@gmail.com>
2014-04-30 12:18:13 -0400

    use 2.1.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'bundler/capistrano'
 require "rvm/capistrano"
 
-set :rvm_ruby_string, "ruby-2.1.0-p0@jonesdeinidotcom"
+set :rvm_ruby_string, "ruby-2.1.1@jonesdeinidotcom"
 
 set :application, "jonesdeinidotcom"
 set :deploy_to, "/home/jonesdeinidotcom"
